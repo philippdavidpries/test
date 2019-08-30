8b366f71ab8372595b88a5ef814e63ed0ef3578d
philippdavidpries <54642218+philippdavidpries@users.noreply.github.com>
2019-08-30 19:39:17 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,4 +1,15 @@
 import scraperwiki
 html=scraperwiki.scrape("http://inmo.ie/6022")
 print("Clickonthe...morelinktoseethewholepage")
-print html
+
+
+ 
+importlxml.html
+root=lxml.html.fromstring(html) #turnourHTMLintoanlxmlobject
+
+tds=root.cssselect('td') #getallthe<tdtags
+fortdintds:
+print lxml.html.tostring(td) # the full HTML tag
+print td.text # just the text inside the HTML tag
+
+
