1bde23a3f8baf5c08a34210d92c2043b2aa00b17
philippdavidpries <54642218+philippdavidpries@users.noreply.github.com>
2019-08-30 20:22:54 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,3 +1,8 @@
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(html_doc, 'html.parser')
+
+
+
 import scraperwiki
 html=scraperwiki.scrape("https://www.walderbach.de/fileadmin/user_upload/Wahl/index.html")
 print("Clickonthe...morelinktoseethewholepage")
@@ -7,7 +12,7 @@ print("Clickonthe...morelinktoseethewholepage")
 import lxml.html
 root=lxml.html.fromstring(html) #turnourHTMLintoanlxmlobject
 
-tds=root.cssselect("td") #getallthe<tdtags
+tds=root.cssselect("mitBorderBottom zweite_spalte links") #getallthe<tdtags
 for td in tds:
 #  print lxml.html.tostring(td) # the full HTML tag
  print td.text # just the text inside the HTML tag
