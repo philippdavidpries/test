75417730c9ce5384a7f12b4b619f384b2e4c4c17
philippdavidpries <54642218+philippdavidpries@users.noreply.github.com>
2019-08-30 19:40:13 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -7,8 +7,8 @@ print("Clickonthe...morelinktoseethewholepage")
 importlxml.html
 root=lxml.html.fromstring(html) #turnourHTMLintoanlxmlobject
 
-tds=root.cssselect('td') #getallthe<tdtags
-fortdintds:
+tds=root.cssselect("td") #getallthe<tdtags
+for td in tds:
 print lxml.html.tostring(td) # the full HTML tag
 print td.text # just the text inside the HTML tag
 
